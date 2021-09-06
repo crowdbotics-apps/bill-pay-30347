cebe33b8a7418b714e4fc68b05285164715fd7a4
Crowdbotics <team@crowdbotics.com>
2021-09-06 08:36:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "bill_pay_30347Identifier",
+  url: "https://bill_pay_30347Identifier.botics.co",
+  api: "https://bill_pay_30347Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
